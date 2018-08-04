7b32dced2396f568d470039d635206b2d5398afb
andreyfoggy <andreyfoggy9009@gmail.com>
2018-08-04 11:44:49 +0300

    cookiesDZ

---
+++ cookies/script.js
@@ -0,0 +1,26 @@
+var userName = document.querySelector("#name")
+var city = document.querySelector("#city")
+var age = document.querySelector("#age")
+var form = document.querySelector("FORM")
+var button_save = document.querySelector("#button_save")
+var button_sign = document.querySelector("#button_sign")
+var fields = [userName,city,age]
+
+button_save.onclick = function(event){
+  fields.forEach(x=>{
+    document.cookie = (`${x.id}=${x.value}`)
+  })
+  form.style.display = "none"
+  button_sign.style.display = "block"
+}
+button_sign.onclick = function(event){
+  form.style.display = "block"
+  event.target.style.display = "none"
+  var cookies = document.cookie.split(";",3).map(x=>x.split("=")[1])
+  var count = 0
+  fields.forEach(x=>{
+    x.value = cookies[count++]
+  })
+}
+
+
